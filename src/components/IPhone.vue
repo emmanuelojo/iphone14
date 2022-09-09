a49efde2b34f89baf6a9e41d92fad070b54f7a76
emmanuelOjo <emmanuelojo1234@gmail.com>
2022-09-09 21:40:52 +0100

    music player added

--- src/components/IPhone.vue
+++ src/components/IPhone.vue
@@ -10,6 +10,9 @@ onBeforeMount(() => {
 onMounted(() => {
   //   const ringtone = new Audio(require('../assets/iphone.mp3'))
   //   const ringtone = new Audio("../assets/iphone.mp3");
+  setTimeout(() => {
+    showMusic.value = true;
+  }, 2000);
 });
 
 const audioRef = ref() as Ref<HTMLAudioElement>;
@@ -26,6 +29,8 @@ const min = ref(0);
 const sec = ref(0);
 
 const toggleNetworkIcons = ref(true);
+
+const showMusic = ref(false);
 
 const activeIsland = ref("initial");
 
@@ -59,9 +64,10 @@ setTimeout(() => {
   toggleNetworkIcons.value = false;
   //   activeIsland.value = "callDetails";
   activeIsland.value = "incomingCall";
+  showMusic.value = false;
   // activeIsland.value = "musicLarge";
   ringtone.play();
-}, 5000);
+}, 10000);
 
 setInterval(currentTime, 1000);
 
@@ -113,7 +119,7 @@ watch(activeIsland, () => {
                   class="h-7 w-[25%] p-1 flex justify-end items-center rounded-2xl bg-black"
                 ></div>
 
-                <div
+                <!-- <div
                   v-if="activeIsland === 'musicLarge'"
                   class="absolute top-[18px] left-4 right-4 bg-black rounded-[30px]"
                 >
@@ -174,7 +180,7 @@ watch(activeIsland, () => {
                       </div>
                     </div>
                   </div>
-                </div>
+                </div> -->
 
                 <div
                   v-if="activeIsland === 'incomingCall'"
@@ -308,6 +314,59 @@ watch(activeIsland, () => {
                 <p class="text-gray-400 text-center">Friday, September 9</p>
               </div>
               <h1 class="text-[70px] text-white">{{ clock }}</h1>
+            </div>
+
+            <div
+              v-if="showMusic"
+              class="absolute bottom-[120px] left-4 right-4 bg-black rounded-[30px]"
+            >
+              <div class="px-3 pt-3 flex justify-between">
+                <div class="flex items-center gap-2">
+                  <img
+                    src="https://i.scdn.co/image/ab67616d0000b2736c32fd4910e4aacea6143e7a"
+                    alt="Music Image"
+                    class="text-[9px] w-14 h-14 rounded-lg object-cover"
+                  />
+
+                  <div>
+                    <p class="text-xs text-white font-semibold">
+                      Spread the ops
+                    </p>
+                    <p class="text-xs text-gray-400 font-medium">Lecrae</p>
+                  </div>
+                </div>
+
+                <span class="material-symbols-outlined text-white">
+                  graphic_eq
+                </span>
+              </div>
+
+              <div class="px-3 pt-2 pb-5 flex flex-col gap-[10px]">
+                <div class="flex items-center gap-2">
+                  <p class="text-gray-400 text-[9px]">2:50</p>
+                  <div class="h-[6px] bg-gray-400 flex flex-grow rounded-lg">
+                    <div class="h-[6px] bg-white rounded-l-full w-4/5"></div>
+                  </div>
+                  <p class="text-gray-400 text-[9px]">-0:51</p>
+                </div>
+
+                <div class="relative flex justify-center text-white">
+                  <div class="flex justify-center items-center gap-5">
+                    <i
+                      class="text-[18px] fa fa-backward"
+                      aria-hidden="true"
+                    ></i>
+                    <i class="text-[18px] fa fa-pause" aria-hidden="true"></i>
+                    <i class="text-[18px] fa fa-forward" aria-hidden="true"></i>
+                  </div>
+
+                  <div class="absolute right-1">
+                    <span class="material-symbols-outlined text-[15px]">
+                      podcasts
+                    </span>
+                  </div>
+                </div>
+              </div>
             </div>
 
             <div class="absolute bottom-3 left-0 right-0 flex flex-col gap-10">
